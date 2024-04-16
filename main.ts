0965fa8baa4ef307ed9780b3e134b897a9151cfc
kristinasamuel <166210993+kristinasamuel@users.noreply.github.com>
2024-04-16 13:39:04 +0500

    CLI based currency converter

---
+++ main.ts
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const currency: any = {
+  USD: 1, // base currency
+  EUR: 0.91,
+  SAR: 3.75,
+  INR: 74.57,
+  PKR: 280,
+};
+
+let userAnswer = await inquirer.prompt([
+  {
+    name: "from",
+    message: "Enter from Currency",
+    type: "list",
+    choices: ["USD", "EUR", "SAR", "INR", "PKR"],
+  },
+  {
+    name: "to",
+    message: "Enter to Curencey",
+    type: "list",
+    choices: ["USD", "EUR", "SAR", "INR", "PKR"],
+  },
+  {
+    name: "amount",
+    message: " Enter your amount",
+    type: "number",
+  },
+]);
+
+let fromAmount = currency[userAnswer.from];
+let toAmount = currency[userAnswer.to];
+let amount = userAnswer.amount;
+let baseAmount = amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
